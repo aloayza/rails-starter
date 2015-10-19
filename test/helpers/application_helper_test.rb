4ba1b3e692c352632c1c9d0ad94793ffe34396a4
Allen Loayza <allen.loayza@gmail.com>
2015-10-19 00:25:06 -0400

    remove unneeded files

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title,         "Sample Rails App"
-    assert_equal full_title("Help"), "Help | Sample Rails App"
-  end
-end
