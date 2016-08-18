173a165b9789a13a5ad85177bafea96ab7a6df04
Allen Loayza <allen.loayza@gmail.com>
2016-08-18 13:14:30 -0400

    Updated help and about pages

--- template.rb
+++ template.rb
@@ -149,13 +149,6 @@ production:
     end
     remove_file 'seeds.rb'
     create_file 'seeds.rb' do <<-EOF
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name: "Allen Loayza",
     email: "allen.loayza@gmail.com",
     password: "123456789",
